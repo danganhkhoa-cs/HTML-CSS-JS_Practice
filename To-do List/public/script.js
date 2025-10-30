4b404f486c1190aedb8adba8c31f01c798fda2a1
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-10-30 12:43:54 +0700

    Completed to-do list web

--- To-do List/public/script.js
+++ To-do List/public/script.js
@@ -1,85 +1,108 @@
 const newTaskButton = document.getElementById("new-task-btn");
 const deleteIcons = document.getElementsByClassName("delete-icon");
 const taskList = document.getElementById("task-list");
-const tasksElement = document.querySelectorAll(".task");
 const createTaskDialog = document.getElementById("create-task-dialog");
-const saveTaskButton = document.getElementById("save-task-button");
+const saveTaskButton = document.getElementById("save-task-btn");
 const cancelButton = document.getElementById("cancel-btn");
+const taskDescriptionInput = document.getElementById("task-description-input");
+const colorInputList = document.querySelectorAll("#color-input input");
 
-const tasks = [
-  { id: "1", description: "Learning Python every week", status: true },
-  { id: "2", description: "Buy oranges for mom", status: false },
-];
+let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
 
-const renderTask = () => {
-  tasks.forEach(({ id, description, status }) => {
+const addNewTask = (id, description, color) => {
+  const task = {
+    id: id,
+    description: description,
+    status: true,
+    color: color,
+  };
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  taskList.innerHTML += `
+    <div class="task-container group/task">
+      <div data-id="${id}" class="task bg-(--${color})">
+        <i class="fa-regular fa-square task-status"></i>
+        <p class="task-description">${description}</p>
+      </div>
+      <i data-id="${id}" class="fa-solid fa-trash delete-icon "></i>
+    </div>
+  `;
+};
+
+const renderTasks = () => {
+  tasks.forEach(({ id, description, status, color }) => {
     taskList.innerHTML += status
       ? `
       <div class="task-container group/task">
-        <div id="${id}" class="task bg-(--rose)">
-          <i class="fa-regular fa-square task-status"></i>
-          <p class="task-description line-through opacity-70">${description}</p>
-        </div>
-        <i class="fa-solid fa-trash delete-icon peer"></i>
-      </div>
-    `
-      : `
-      <div class="task-container group/task">
-        <div id="${id}" class="task bg-(--rose)">
+        <div data-id="${id}" class="task bg-(--${color})">
           <i class="fa-regular fa-square task-status"></i>
           <p class="task-description">${description}</p>
         </div>
-        <i class="fa-solid fa-trash delete-icon peer"></i>
+        <i data-id="${id}" class="fa-solid fa-trash delete-icon "></i>
+      </div> `
+      : `
+      <div class="task-container group/task">
+        <div data-id="${id}" class="task bg-(--${color})">
+          <i class="fa-solid fa-square-check task-status"></i>
+          <p class="task-description line-through opacity-70">${description}</p>
+        </div>
+        <i data-id="${id}" class="fa-solid fa-trash delete-icon "></i>
       </div>
     `;
   });
 };
-
-const createTask = () => {};
+renderTasks();
 
 newTaskButton.addEventListener("click", () => {
   createTaskDialog.showModal();
+});
+
+saveTaskButton.addEventListener("click", () => {
+  const description = taskDescriptionInput.value;
+  const color = Array.from(colorInputList).find(
+    (input) => input.checked == true
+  ).value;
+  const id = Date.now().toString();
+
+  addNewTask(id, description, color);
+
+  taskDescriptionInput.value = "";
+  createTaskDialog.close();
 });
 
 cancelButton.addEventListener("click", () => {
   createTaskDialog.close();
 });
 
-tasksElement.forEach((element) => {
-  element.addEventListener("click", () => {
-    const currentTask = tasks.find((task) => task.id === element.id);
-    const description = element.querySelector("p");
-    const statusIcon = element.querySelector("i");
+taskList.addEventListener("click", (event) => {
+  const taskElement = event.target.closest(".task");
+  const deleteIcon = event.target.closest(".delete-icon");
 
-    if (currentTask.status) {
-      description.classList.add("line-through", "opacity-70");
+  if (taskElement) {
+    const currentTaskObject = tasks.find(
+      (task) => task.id === taskElement.dataset.id
+    );
+    const descriptionElement = taskElement.querySelector("p");
+    const statusIcon = taskElement.querySelector("i");
+
+    if (currentTaskObject.status) {
+      descriptionElement.classList.add("line-through", "opacity-70");
       statusIcon.classList.add("fa-solid", "fa-square-check");
       statusIcon.classList.remove("fa-regular", "fa-square");
     } else {
-      description.classList.remove("line-through", "opacity-70");
+      descriptionElement.classList.remove("line-through", "opacity-70");
       statusIcon.classList.remove("fa-solid", "fa-square-check");
       statusIcon.classList.add("fa-regular", "fa-square");
     }
-    currentTask.status = !currentTask.status;
-  });
+    currentTaskObject.status = !currentTaskObject.status;
+  }
+
+  if (deleteIcon) {
+    tasks = tasks.filter((task) => task.id !== deleteIcon.dataset.id);
+    toRemoveTaskContainer = deleteIcon.closest(".task-container");
+    toRemoveTaskContainer.remove();
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 });
-
-{
-  /* <div class="task-container group/task">
-  <div class="task bg-(--rose)">
-    <i class="fa-regular fa-square task-status"></i>
-    <p class="task-description">Learning Python every week</p>
-  </div>
-  <i class="fa-solid fa-trash delete-icon peer"></i>
-</div> */
-}
-
-// <div class="task-container group/task">
-//   <div class="task bg-(--flamingo)">
-//     <i class="fa-solid fa-square-check task-status"></i>
-//     <p class="task-description line-through opacity-70">
-//       Buy oranges for mom
-//     </p>
-//   </div>
-//   <i class="fa-solid fa-trash delete-icon peer"></i>
-// </div>
