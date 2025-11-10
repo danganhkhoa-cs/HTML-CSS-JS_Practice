cd4ee54875d41e843bc0f2460240dde0a055e740
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-11-10 08:39:17 +0700

    Initialize Unit Converter website

---
+++ Unit Converter/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  base: "/HTML-CSS-JS_Practice/Unit Converter/",
+});
