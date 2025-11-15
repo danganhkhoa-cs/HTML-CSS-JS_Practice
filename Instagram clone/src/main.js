c9db455679f9f264df65c2f969d4559456ed8ad0
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-11-15 11:41:14 +0700

    Completed Instagram clone website

--- Instagram clone/src/main.js
+++ Instagram clone/src/main.js
@@ -1,32 +1,66 @@
 import "./style.css";
+import { posts } from "/src/posts.js";
 
-const posts = [
-  {
-    name: "Vincent van Gogh",
-    username: "vincey1853",
-    location: "Zundert, Netherlands",
-    avatar: "images/avatar-vangogh.jpg",
-    post: "images/post-vangogh.jpg",
-    comment: "just took a few mushrooms lol",
-    likes: 21,
-  },
-  {
-    name: "Gustave Courbet",
-    username: "gus1819",
-    location: "Ornans, France",
-    avatar: "images/avatar-courbet.jpg",
-    post: "images/post-courbet.jpg",
-    comment: "i'm feelin a bit stressed tbh",
-    likes: 4,
-  },
-  {
-    name: "Joseph Ducreux",
-    username: "jd1735",
-    location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
-    comment:
-      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-    likes: 152,
-  },
-];
+const mainContainer = document.getElementById("main");
+
+/**
+ * @param {import('./posts.js').Post[]} posts
+ */
+const renderPosts = (posts) => {
+  let htmlText = "";
+  for (const post of posts) {
+    htmlText += `
+      <div class="post">
+          <div class="post-header">
+            <img class="avatar" src="${post.avatar}" />
+            <div class="user-info">
+              <p class="name">${post.name}</p>
+              <p class="location">${post.location}</p>
+            </div>
+          </div>
+          <div class="post-content">
+            <img src="${post.post}" class="post-img" />
+          </div>
+          <div class="post-interaction">
+            <div>
+              <img
+                src="icon-heart.png"
+                class="heart-icon interaction-icon w-6"
+              />
+              <img src="icon-comment.png" class="interaction-icon w-5" />
+              <img src="icon-dm.png" class="interaction-icon w-6" />
+            </div>
+            <p class="font-bold">${post.likes} likes</p>
+            <p class="comment">
+              <span class="username">${post.username}</span> ${post.comment}
+            </p>
+          </div>
+        </div>
+    `;
+  }
+  mainContainer.innerHTML = htmlText;
+};
+
+renderPosts(posts);
+
+mainContainer.addEventListener("click", (event) => {
+  const heartIcon = event.target.closest(".heart-icon");
+
+  if (heartIcon) {
+    heartIcon.src = heartIcon.src.includes("icon-heart-red.png")
+      ? "icon-heart.png"
+      : "icon-heart-red.png";
+  }
+});
+
+mainContainer.addEventListener("dblclick", (event) => {
+  const postContent = event.target.closest(".post-content");
+
+  if (postContent) {
+    const post = postContent.closest(".post");
+    const heartIcon = post.querySelector(".heart-icon");
+    heartIcon.src = heartIcon.src.includes("icon-heart-red.png")
+      ? "icon-heart.png"
+      : "icon-heart-red.png";
+  }
+});
