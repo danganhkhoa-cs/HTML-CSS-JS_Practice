bcb14f8efffd04cc19fc967737c56e5b2ff08be5
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-11-15 14:31:36 +0700

    Enhance accessibility

--- Instagram clone/src/main.js
+++ Instagram clone/src/main.js
@@ -10,25 +10,27 @@ const renderPosts = (posts) => {
   let htmlText = "";
   for (const post of posts) {
     htmlText += `
-      <div class="post">
+      <li>
+        <div class="post">
           <div class="post-header">
-            <img class="avatar" src="${post.avatar}" />
+            <img class="avatar" src="${post.avatar}" alt="Avatar of ${post.name}"/>
             <div class="user-info">
               <p class="name">${post.name}</p>
               <p class="location">${post.location}</p>
             </div>
           </div>
           <div class="post-content">
-            <img src="${post.post}" class="post-img" />
+            <img src="${post.post}" class="post-img" alt="Image that ${post.name} has posted"/>
           </div>
           <div class="post-interaction">
             <div>
               <img
                 src="icon-heart.png"
                 class="heart-icon interaction-icon w-6"
+                alt="Heart icon for loving the post"
               />
-              <img src="icon-comment.png" class="interaction-icon w-5" />
-              <img src="icon-dm.png" class="interaction-icon w-6" />
+              <img src="icon-comment.png" class="interaction-icon w-5" alt="Icon to open comments section"/>
+              <img src="icon-dm.png" class="interaction-icon w-6" alt="Sharing icon"/>
             </div>
             <p class="font-bold">${post.likes} likes</p>
             <p class="comment">
@@ -36,6 +38,7 @@ const renderPosts = (posts) => {
             </p>
           </div>
         </div>
+      </li>
     `;
   }
   mainContainer.innerHTML = htmlText;
