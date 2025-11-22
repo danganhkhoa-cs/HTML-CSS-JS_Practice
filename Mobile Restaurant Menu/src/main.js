19f56e715929eb1e497f01dc6907ca57acac5605
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-11-22 08:45:00 +0700

    Completed UI design for rendering food list

--- Mobile Restaurant Menu/src/main.js
+++ Mobile Restaurant Menu/src/main.js
@@ -1,2 +1,28 @@
 import "./style.css";
 import { menuArray } from "./data";
+
+const foodList = document.getElementById("food-list");
+
+const renderFood = (menuArray) => {
+	const htmlText = menuArray
+		.map(
+			({ name, ingredients, id, price, emoji }) => `
+        <li id="${id}" class="item">
+            <p class="emoji">${emoji}</p>
+            <div class="description">
+                <p class="name">${name}</p>
+                <p class="ingredients">${ingredients.join(", ")}</p>
+                <p class="price">$${price}</p>
+            </div>
+            <button class="add-btn" data-food-id="${id}">
+                <i class="fa-solid fa-plus"></i>
+            </button>
+        </li>
+    `
+		)
+		.join("<div class='divider'></div>");
+
+	foodList.innerHTML = htmlText;
+};
+
+renderFood(menuArray);
