32046a99d85b96012069b1fc85a4550996bb8d44
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-11-25 22:29:55 +0700

    Add navigation toggle for  small screen device

--- Portfolio/src/main.js
+++ Portfolio/src/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+const navigationToggle = document.getElementById("navigation-toggle");
+const navigationMobile = document.getElementById("navigation-mobile");
+const navigationBackdrop = document.getElementById("navigation-backdrop");
+
+// Navigation toggle logic
+const closeNavigationMenu = () => {
+	navigationToggle.checked = false;
+	navigationBackdrop.style.display = "none";
+};
+navigationToggle.addEventListener("change", () => {
+	navigationToggle.checked
+		? (navigationBackdrop.style.display = "block")
+		: (navigationBackdrop.style.display = "none");
+});
+navigationMobile.addEventListener("click", closeNavigationMenu);
+navigationBackdrop.addEventListener("click", closeNavigationMenu);
