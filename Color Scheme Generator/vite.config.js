db2fd832a63a373fcd51ae2bed481b88f346c46a
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-12-03 22:14:01 +0700

    Completed design for Color Scheme Generator web

---
+++ Color Scheme Generator/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+	plugins: [tailwindcss()],
+	base: "/HTML-CSS-JS_Practice/Color Scheme Generator/",
+});
