303db237fbe2548f8e0a41853d3931b18d8ef6c2
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-12-04 08:47:22 +0700

    Completed design and logic for website

--- Color Scheme Generator/src/main.js
+++ Color Scheme Generator/src/main.js
@@ -11,8 +11,13 @@ const modeList = [
 	"Quad",
 ];
 
+let selectedMode = "";
+let selectedColor = "#000000";
+
+const colorInput = document.getElementById("color-input");
 const modeInput = document.getElementById("mode-input");
 const modeSelector = document.querySelector(".mode-selector");
+const getSchemeButton = document.getElementById("get-scheme-btn");
 
 const renderModes = (modeList) => {
 	const htmlText = modeList
@@ -44,17 +49,52 @@ const renderModes = (modeList) => {
 };
 renderModes(modeList);
 
+const renderColorScheme = () => {
+	fetch(
+		`https://www.thecolorapi.com/scheme?hex=${selectedColor.slice(
+			1
+		)}&mode=${selectedMode}&count=5`
+	)
+		.then((res) => res.json())
+		.then((data) => {
+			const colorObjectList = data.colors;
+			const htmlText = colorObjectList
+				.map(
+					(color) => `
+				<li>
+					<div class="color-div">
+						<div class="color" style="background-color:${color.hex.value}"></div>
+						<p class="color-code">${color.hex.value.toUpperCase()}</p>
+					</div>
+				</li>
+			`
+				)
+				.join("");
+			document.querySelector(".output-list").innerHTML = htmlText;
+		});
+};
+
 modeInput.addEventListener("click", (e) => {
 	const isInsideList = e.target.closest(".mode-selector");
-
 	if (isInsideList) {
 		if (e.target.tagName === "INPUT") {
-			modeSelector.classList.add("hidden");
-
+			//Set text to display
 			const modeLabel = document.querySelector("#mode-input p");
-			modeLabel.textContent = e.target.value;
+			modeLabel.textContent = e.target.closest("label").textContent;
+
+			//Bind mode to global variable
+			selectedMode = e.target.value;
+
+			//Hide selection menu
+			modeSelector.classList.add("hidden");
 		}
 	} else {
 		modeSelector.classList.toggle("hidden");
 	}
 });
+
+colorInput.addEventListener("change", () => {
+	selectedColor = colorInput.value;
+});
+
+getSchemeButton.addEventListener("click", renderColorScheme);
