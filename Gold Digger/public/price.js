d52e44bcc231bc41bf48751b78783315b6dcfc06
danganhkhoa-cs <anhkhoa200511@gmail.com>
2025-12-18 18:36:46 +0700

    Add logic to random price

---
+++ Gold Digger/public/price.js
@@ -0,0 +1,12 @@
+const eventSource = new EventSource("/");
+const priceDisplay = document.getElementById("price-display");
+priceDisplay.textContent = "500";
+
+eventSource.onmessage = (event) => {
+	const data = JSON.parse(event.data);
+	const price = data.price;
+	priceDisplay.textContent = price;
+};
+eventSource.onerror = () => {
+	console.log("Error...");
+};
