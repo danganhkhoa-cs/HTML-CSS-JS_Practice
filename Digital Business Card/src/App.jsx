c60b9a2ed8bfe0e2d292cf2bba3575c6db8387a2
danganhkhoa-cs <anhkhoa200511@gmail.com>
2026-01-09 22:46:56 +0700

    Add dark theme

--- Digital Business Card/src/App.jsx
+++ Digital Business Card/src/App.jsx
@@ -3,8 +3,13 @@ import { Main } from "./components/Main.jsx";
 import { Footer } from "./components/Footer.jsx";
 
 export const App = () => (
-	<div className="w-screen h-screen bg-[#23252c] flex items-center justify-center">
-		<div className="flex flex-col justify-between w-79.25 h-195 bg-[#f5f5f5] rounded-xl overflow-hidden font-['Inter',sans-serif] text-[#4A4E74]">
+	<div className="relative w-screen h-screen bg-[#23252c] flex items-center justify-center gap-10">
+		<div className="flex flex-col justify-between w-79.25 h-195 bg-primary rounded-xl overflow-hidden font-['Inter',sans-serif] text-text">
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+		<div className="flex flex-col justify-between w-79.25 h-195 dark bg-primary rounded-xl overflow-hidden font-['Inter',sans-serif] text-text">
 			<Header />
 			<Main />
 			<Footer />
