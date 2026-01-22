3d610053269e9de80d9909b574ef8c48c906edaa
Dang Anh Khoa <anhkhoa200511@gmail.com>
2026-01-22 11:53:01 +0700

    Completed project

--- Quizzical App/src/components/Question/Question.jsx
+++ Quizzical App/src/components/Question/Question.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import { clsx } from "clsx";
+import { ClipLoader } from "react-spinners";
 
 import { Blob } from "./Blob";
 
-import { shuffle } from "../../utils/shuffle.js";
-
-export function Questions({ questions }) {
+export function Questions({ questions, refetchQuestions }) {
 	// State value
+	const [isLoading, setIsloading] = useState(false);
 	const [answers, setAnswers] = useState({});
 	const [isChecked, setIsChecked] = useState(false);
 
 	// Derived value
+	const correctAnswersCount = Object.values(answers).reduce(
+		(count, ans, index) =>
+			ans === questions[index].correct_answer ? count + 1 : count,
+		0,
+	);
 	const questionElements = questions.map(
 		({ question, correct_answer, options }, index) => {
 			const inputElements = options.map((option) => {
@@ -56,18 +61,50 @@ export function Questions({ questions }) {
 	function checkAnswer() {
 		setIsChecked(true);
 	}
+	async function playAgain() {
+		setIsloading(true);
+		await refetchQuestions();
+		setIsChecked(false);
+		setAnswers({});
+		setIsloading(false);
+	}
+	function renderActionButton() {
+		if (isChecked) {
+			if (isLoading) {
+				return <ClipLoader loading={true} color={"#4D5B9E"} />;
+			} else {
+				return (
+					<>
+						<p>You scored {correctAnswersCount}/5 correct answers</p>
+						<button
+							onClick={playAgain}
+							className="bg-[#4D5B9E] text-[#F5F7FB] text-[0.8rem] py-2 px-8 rounded-[10px] font-medium cursor-pointer hover:bg-[#475289]"
+						>
+							Play again
+						</button>
+					</>
+				);
+			}
+		} else {
+			return (
+				<button
+					onClick={checkAnswer}
+					className="bg-[#4D5B9E] text-[#F5F7FB] text-[0.8rem] py-2 px-6 rounded-[10px] font-medium cursor-pointer hover:bg-[#475289]"
+				>
+					Check answers
+				</button>
+			);
+		}
+	}
 	return (
 		<section className="relative w-full z-0">
 			<Blob />
 			{/* Questions */}
 			<div className="container">
 				<ul className="question-list">{questionElements}</ul>
-				<button
-					onClick={checkAnswer}
-					className="bg-[#4D5B9E] text-[#F5F7FB] text-[0.8rem] py-2 px-6 mt-5 rounded-[10px] font-medium cursor-pointer hover:bg-[#475289]"
-				>
-					Check answers
-				</button>
+				<div className="flex items-center justify-center mt-5 gap-3">
+					{renderActionButton()}
+				</div>
 			</div>
 		</section>
 	);
